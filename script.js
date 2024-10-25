505ec39628a34b73dba230dfb55e94ea615ee0a8
aratz <egurenaratz@gmail.com>
2024-10-25 08:10:10 +0200

    Fallo encontrado

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.getElementById('toggleSidebar').addEventListener('click', function() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('closed');
+
+    const toggleButton = document.getElementById('toggleSidebar');
+    if (sidebar.classList.contains('closed')) {
+        
+        toggleButton.style.marginLeft = '210px'; 
+        toggleButton.style.backgroundColor = '#001F3F'; 
+    } else {
+        
+        toggleButton.style.marginLeft = '0px'; 
+        toggleButton.style.backgroundColor = '#3A6D8C'; 
+    }
 });
